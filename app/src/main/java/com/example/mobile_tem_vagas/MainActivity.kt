d0eeeb7fc7d0fedb69c24a2444929ba2f7e7b1cc
Luis Felipe Valadares Pantoja <89617375+FelipeLuis123@users.noreply.github.com>
2024-02-06 15:49:24 -0300

    commit da tela de login

---
+++ app/src/main/java/com/example/mobile_tem_vagas/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.mobile_tem_vagas
+
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Looper
+import android.view.View
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobile_tem_vagas.ui.theme.MobileTemVagasTheme
+import com.example.mobile_tem_vagas.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import android.os.Handler
+import android.content.Intent
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        window.statusBarColor = Color.parseColor("#FFFFFF")
+
+        binding.btEntrar.setOnClickListener{
+
+            val user = binding.editUser.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when{
+                user.isEmpty() -> {
+                    binding.editUser.error="Preencha o Usuário!"
+                }
+                senha.isEmpty() -> {
+                    binding.editSenha.error="Preencha a Senha!"
+                }
+                senha.length <= 5 -> {
+                    val snackbar = Snackbar.make(it, "A senha precisa ter pelo menos 6 caracteres!", Snackbar.LENGTH_SHORT)
+                    snackbar.show()
+                }
+                else ->{
+                    login(it)
+                }
+            }
+        }
+    }
+    private fun login(view :View) {
+        val progressbar = binding.progressBar
+        progressbar.visibility = View.VISIBLE
+
+        binding.btEntrar.isEnabled = false
+        binding.btEntrar.setTextColor(Color.parseColor("#FFFFFF"))
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            navegarTelaPrincipal()
+            val snackbar = Snackbar.make(view, "Login efetuado com sucesso!", Snackbar.LENGTH_SHORT)
+            snackbar.show()
+        }, 3000)
+    }
+    private fun navegarTelaPrincipal(){
+        val intent = Intent(this, Telaprincipal::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
+
---
+++ app/src/main/java/com/example/mobile_tem_vagas/Telaprincipal.kt
@@ -0,0 +1,15 @@
+package com.example.mobile_tem_vagas
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Telaprincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_telaprincipal)
+
+        supportActionBar!!. hide()
+        window.statusBarColor = Color.parseColor("#5B00FF")
+    }
+}
