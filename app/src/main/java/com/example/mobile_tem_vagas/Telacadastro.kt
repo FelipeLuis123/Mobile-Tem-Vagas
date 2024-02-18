f1f9907da927e126d86ec5c69876add27f4bce12
Luis Felipe Valadares Pantoja <89617375+FelipeLuis123@users.noreply.github.com>
2024-02-18 20:18:45 -0300

    criado a notificação na tela de cadastro.

--- app/src/main/java/com/example/mobile_tem_vagas/Telacadastro.kt
+++ app/src/main/java/com/example/mobile_tem_vagas/Telacadastro.kt
@@ -1,15 +1,26 @@
 package com.example.mobile_tem_vagas
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mobile_tem_vagas.databinding.ActivityTelacadastroBinding
 import com.google.android.material.snackbar.Snackbar
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class Telacadastro : AppCompatActivity() {
     private lateinit var binding: ActivityTelacadastroBinding
+
+    private val CHANNEL_ID = "TemVagasChannel"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +31,9 @@ class Telacadastro : AppCompatActivity() {
 
         // Configurações da sua activity...
         window.statusBarColor = Color.parseColor("#5B00FF")
+
+        // Criar o canal de notificação
+        createNotificationChannel()
 
         binding.btCadastrar.setOnClickListener {
             val nome = binding.editNome.text.toString()
@@ -45,6 +59,7 @@ class Telacadastro : AppCompatActivity() {
                 else -> {
                     val intent = Intent(this, Telaprincipal::class.java)
                     startActivity(intent)
+                    showNotification()
                 }
             }
         }
@@ -62,5 +77,43 @@ class Telacadastro : AppCompatActivity() {
         val telaprincipal = Intent(this, Telaprincipal::class.java)
         startActivity(telaprincipal)
         finish()
+    }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, "Primeiro canal",
+                NotificationManager.IMPORTANCE_DEFAULT)
+                channel.description = "Testando meu primeiro canal"
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    // Exibir notificação push
+    private fun showNotification() {
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_casa)
+            .setContentTitle(getString(R.string.notification_title))
+            .setContentText(getString(R.string.notification_text))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId é um número único para cada notificação que você deve definir
+            if (ActivityCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            notify(1, builder.build())
+        }
     }
 }
